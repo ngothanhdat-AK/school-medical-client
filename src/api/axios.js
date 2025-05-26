27b1388a3d1afa0a2bf46ed8562f4c675a045798
ngothanhdat-AK <ngothanhdat4002@gmail.com>
2025-05-26 22:17:09 +0700

    feat: add antd dependency and implement MainLayout component
    - Added antd library to package.json for UI components.
    - Introduced MainLayout component to structure admin dashboard layout.
    - Updated App component to use MainLayout for admin route.
    - Refactored axios.js to create a single axios instance for API calls.
    - Updated Login component to use authenticationAPI for login requests.
    - Created authentication service for handling authentication-related API calls.

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,23 +1,14 @@
 import axios from "axios";
 
 // API public - không cần token
-const axiosPublic = axios.create({
+const axiosInstance = axios.create({
   baseURL: "https://localhost:7009/api",
   headers: {
     "Content-Type": "application/json",
   },
 });
 
-// API protected - tự động đính kèm token Authorization
-const axiosPrivate = axios.create({
-  baseURL: "https://localhost:7009/api",
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
-
-// Interceptor tự động đính kèm token từ localStorage vào header
-axiosPrivate.interceptors.request.use(
+axiosInstance.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("accessToken");
     if (token) {
@@ -28,4 +19,15 @@ axiosPrivate.interceptors.request.use(
   (error) => Promise.reject(error)
 );
 
-export { axiosPublic, axiosPrivate };
+export default axiosInstance;
+
+
+// API protected - tự động đính kèm token Authorization
+// const axiosPrivate = axios.create({
+//   baseURL: "https://localhost:7009/api",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// });
+
+// // Interceptor tự động đính kèm token từ localStorage vào header
